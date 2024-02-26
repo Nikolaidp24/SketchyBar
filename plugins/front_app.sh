b7ede26a2bd849a9b1e597a5283c20fbae9390e4
Nikolai Peisong Li <coopakamy@gmail.com>
2024-02-26 15:08:35 +0800

    turn off label for front app

--- plugins/front_app.sh
+++ plugins/front_app.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 if [ "$SENDER" = "front_app_switched" ]; then
-	sketchybar --set "$NAME" label="$INFO" icon.background.image="app.$INFO"
+	sketchybar --set "$NAME" icon.background.image="app.$INFO"
 fi
