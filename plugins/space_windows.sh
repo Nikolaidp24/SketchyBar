9374076867afe6f479ad08c551f6068cc795e74a
Nikolai Peisong Li <coopakamy@gmail.com>
2024-03-07 09:07:17 +0800

    added TODO

--- plugins/space_windows.sh
+++ plugins/space_windows.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
+# TODO: Microsoft Outlook along with couple other
+# applications are not read properly at the moment
 
 if [ "$SENDER" = "space_windows_change" ]; then
-  space="$(echo "$INFO" | jq -r '.space')"
-  apps="$(echo "$INFO" | jq -r '.apps | keys[]')"
+	space="$(echo "$INFO" | jq -r '.space')"
+	apps="$(echo "$INFO" | jq -r '.apps | keys[]')"
 
-  icon_strip=" "
-  if [ "${apps}" != "" ]; then
-    while read -r app
-    do
-      icon_strip+=" $($CONFIG_DIR/plugins/icon_map.sh "$app")"
-    done <<< "${apps}"
-  else
-    icon_strip=" —"
-  fi
+	icon_strip=" "
+	if [ "${apps}" != "" ]; then
+		while read -r app; do
+			icon_strip+=" $($CONFIG_DIR/plugins/icon_map.sh "$app")"
+		done <<<"${apps}"
+	else
+		icon_strip=" —"
+	fi
 
-  sketchybar --animate sin 10 --set space.$space label="$icon_strip"
+	sketchybar --animate sin 10 --set space.$space label="$icon_strip"
 fi
